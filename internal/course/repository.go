5e5515922485ed1d79e2208a2aae36e515b29c30
Hoang Nghi <hoangnghinguyen17@gmail.com>
2026-07-02 14:14:49 +0700

    save

--- internal/course/repository.go
+++ internal/course/repository.go
@@ -58,9 +58,9 @@ func (repository *Repository)FindAll(cursor uint, topicIDs []uint, level string 
 func (repository *Repository)GetMyCourses(userID uint)([]Course, error){
 	var courses []Course
 	var user *user.User
-	err1 := repository.db.Joins("Profile").Where("users.id = ?", userID).Find(&user).Error
+	err1 := repository.db.Joins("Profile").Where("users.id = ?", userID).First(&user).Error
 	if err1 == nil{
-		err := repository.db.Where("author_id = ?", user.Profile.ID).Find(&courses).Error
+		err := repository.db.Joins("Author").Where("author_id = ?", user.Profile.ID).Find(&courses).Error
 		if err != nil{
 			return nil, err
 		}
